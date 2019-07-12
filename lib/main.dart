bf6ac8f415073424dc9da3d1432bdd3b7c57d054
crmgunter <crmgunter@gmail.com>
2019-07-12 17:06:57 -0400

    change state of favorites on press of favorite button

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,47 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
+
+class FavoriteWidget extends StatefulWidget {
+  @override
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+
+  _toggleFavorite() {
+    setState(() {
+      _isFavorited = !_isFavorited;
+      _favoriteCount = _isFavorited? _favoriteCount + 1 : _favoriteCount - 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: Container(
+            child: Text('$_favoriteCount'),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
@@ -33,11 +74,7 @@ class MyApp extends StatelessWidget {
               ],
             ),
           ),
-          Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          Text('41'),
+          FavoriteWidget(),
         ],
       ),
     );
